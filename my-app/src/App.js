938516639f5504361f0633f9fb38ac1d78ed33a2
MikhailZheltovskikh <misha14051994@mail.ru>
2024-04-01 00:28:46 +0500

    задание выполнил

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './app.module.css';
+import { useState } from 'react';
 
 export const App = () => {
+	const [value, setValue] = useState(0);
+	const [list, setList] = useState([]);
+	const [error, setError] = useState(0);
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt('Введите значение');
+
+		if (promptValue.length >= 3) {
+			setValue(promptValue);
+			setError('');
+		} else {
+			setError('Введенное значение должно содержать минимум 3 символа');
+		}
+	};
+
+	let isValueVaild;
+	if (value.length >= 3) {
+		isValueVaild = true;
+	} else {
+		isValueVaild = false;
+	}
+
+	const onAddButtonClick = () => {
+		if (value.length >= 3) {
+			const updatedList = [
+				...list,
+				{ id: Date.now(), value: value.trim(), date: new Date() },
+			];
+			setList(updatedList);
+			setValue('');
+			setError('');
+		}
+	};
+
+	const formatDate = (date) => {
+		const day = date.getDate();
+		const month = date.getMonth() + 1;
+		const year = date.getFullYear();
+		const hours = date.getHours();
+		const minutes = date.getMinutes();
+		const seconds = date.getSeconds();
+		const formattedDate = `${day}.${month}.${year}, ${hours}:${minutes}:${seconds}`;
+		return formattedDate;
+	};
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload .
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
+		<div className={styles.app}>
+			<h1 className={styles['page-heading']}>Ввод значения</h1>
+			<p className={styles['no-margin-text']}>
+				Текущее значение <code>value</code>: "
+				<output className={styles['current-value']}>{value}</output>"
+			</p>
+			{error !== '' && <div className={styles.error}>{error}</div>}
+			<div className={styles['buttons-container']}>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button
+					className={styles.button}
+					disabled={!isValueVaild}
+					onClick={onAddButtonClick}
 				>
-					Learn React
-				</a>
-			</header>
+					Добавить в список
+				</button>
+			</div>
+			<div className={styles['list-container']}>
+				<h2 className={styles['list-heading']}>Список:</h2>
+
+				{list.length > 0 ? (
+					<ul className={styles.list}>
+						{list.map((item) => (
+							<li key={item.id}>
+								{item.value} <span>дата: {formatDate(item.date)}</span>
+							</li>
+						))}
+					</ul>
+				) : (
+					<p className={styles['no-margin-text']}>Нет добавленных элементов</p>
+				)}
+			</div>
 		</div>
 	);
 };
